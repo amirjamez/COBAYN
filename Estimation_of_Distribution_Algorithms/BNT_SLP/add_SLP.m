759bbf84a6f233f55ce6851bf2262b672e8f0758
Amir H. Ashouri <amirhossein.ashouri@gmail.com>
2017-07-31 08:39:33 -0400

    Update add_SLP.m

--- Estimation_of_Distribution_Algorithms/BNT_SLP/add_SLP.m
+++ Estimation_of_Distribution_Algorithms/BNT_SLP/add_SLP.m
@@ -1,5 +1,7 @@
 % Give here the folder where you have uncompressed the Structure Learning Package for BNT
-SLP_HOME = '~/Documents/phd_stuffs/phd_scripts/Bayesian_Model/Estimation_of_Distribution_Algorithms/BNT_SLP';
+% Amir: change this path according:
+
+SLP_HOME = '~/COBAYN/Estimation_of_Distribution_Algorithms/BNT_SLP';
 
 folders = {'learning','scoring','misc','examples','datas','working','working/modded_LinkStrength','working/elr','working/score_dag.c'};
 addpath(SLP_HOME)
